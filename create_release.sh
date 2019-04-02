4f7e1d6187345bd44d875e4cf3541f8bf150405e
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-04-02 15:06:56 +0100

    build(release): impoved release script

--- create_release.sh
+++ create_release.sh
@@ -3,26 +3,22 @@ set -e
 
 git checkout master
 
-VERSION_TAG=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
+CURRENTBRANCHNAME="$(git rev-parse --abbrev-ref HEAD)"
+echo "Working branch: $CURRENTBRANCHNAME"
 
-echo "Check to see if the version has changed"
+echo "Creating new change log"
+./node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s -r 0
 
-if ! git rev-parse $VERSION_TAG >/dev/null 2>&1; then
+echo "Commit change log"
+git add CHANGELOG.md
+git commit -m "chore(release): prepare for release"
 
-    echo "Creating new change log"
+echo "Select type of release?"
 
-    ./node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s -r 0
-    git add CHANGELOG.md
-    git commit -m "chore(release): prepare for release $VERSION_TAG"
-
-    echo "Creating new tag: $VERSION_TAG"
-    # Create a new tag and push to Github
-    git tag $VERSION_TAG
-else
-  echo "Not creating a new tag or CHANGELOG.md as the tag already exists..."
-fi
+select releases in "Major" "Minor" "Patch"; do
+  case $releases in
+    Major ) echo "Updating version " && npm version major -m "chore(release): package version updated and tagged as %s"; break;;
+    Minor ) echo "Updating version " && npm version minor -m "chore(release): package version updated and tagged as %s"; break;;
+    Patch ) echo "Updating version " && npm version patch -m "chore(release): package version updated and tagged as %s"; break;;
+  esac
+done
