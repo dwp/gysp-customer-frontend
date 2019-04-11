d79cd16ece56e9c76b1bca69d03c9e6681c4753e
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-04-11 12:25:46 +0100

    chore(release): seprate change log script

--- create_release.sh
+++ create_release.sh
@@ -6,13 +6,6 @@ git checkout master
 CURRENTBRANCHNAME="$(git rev-parse --abbrev-ref HEAD)"
 echo "Working branch: $CURRENTBRANCHNAME"
 
-echo "Creating new change log"
-./node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s -r 0
-
-echo "Commit change log"
-git add CHANGELOG.md
-git commit -m "chore(release): prepare for release"
-
 echo "Select type of release?"
 
 select releases in "Major" "Minor" "Patch"; do
