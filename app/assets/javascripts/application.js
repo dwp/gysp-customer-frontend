c4f099eba05761846ad8e9ae3e5824bfcc74daec
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-07-01 13:33:59 +0100

    chore(declaration): fixed issue with focus after disable button
    Jira SA-1736
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,6 +59,8 @@ $(window).load(function() {
       // only disable button if they contain 'double-submit-protect' data attribute
       if ($button.data('double-submit-protect')) {
         $button.prop('disabled', true);
+        $button.prop('aria-disabled', true);
+        $button.addClass('govuk-button--disabled');
         $button.parent().attr('tabindex', '-1').focus(); // this is so page is not re-read in JAWS
       }
     });
