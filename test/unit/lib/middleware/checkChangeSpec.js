585919148fc0c57fa54051eb40da9eb5a7784bc3
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2020-05-07 11:29:50 +0100

    chore(scope): check change
    Jira SA-
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- test/unit/lib/middleware/checkChangeSpec.js
+++ test/unit/lib/middleware/checkChangeSpec.js
@@ -18,42 +18,42 @@ describe('check change middleware', () => {
   });
 
   it('should do nothing as there is an empty request', () => {
-    checkChange('/customerfrontend')(emptyRequest, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(emptyRequest, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '');
   });
   it('should redirect when session has a changed marital-select edit section and path is not allowed', () => {
     const request = { session: { editSection: 'marital-select', editSectionChanged: true }, path: '/customerfrontend/check-details' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '/what-is-your-current-marital-status');
   });
   it('should redirect when session has a changed lived-abroad edit section and path is not allowed', () => {
     const request = { session: { editSection: 'lived-abroad', editSectionChanged: true }, path: '/customerfrontend/check-details' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '/have-you-ever-lived-outside-of-the-uk');
   });
   it('should redirect when session has a changed worked-abroad edit section and path is not allowed', () => {
     const request = { session: { editSection: 'worked-abroad', editSectionChanged: true }, path: '/customerfrontend/check-details' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '/have-you-worked-outside-of-the-uk');
   });
   it('should redirect when session has a changed marital-select edit section and path is allowed', () => {
     const request = { session: { editSection: 'marital-select', editSectionChanged: true }, path: '/customerfrontend/what-is-your-current-marital-status' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '');
   });
   it('should redirect when session has a changed lived-abroad edit section and path is allowed', () => {
     const request = { session: { editSection: 'lived-abroad', editSectionChanged: true }, path: '/customerfrontend/have-you-ever-lived-outside-of-the-uk' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '');
   });
   it('should redirect when session has a changed worked-abroad edit section and path is allowed', () => {
     const request = { session: { editSection: 'worked-abroad', editSectionChanged: true }, path: '/customerfrontend/have-you-worked-outside-of-the-uk' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '');
   });
   it('should do nothing as protected section does not match', () => {
     const request = { session: { editSection: 'bob', editSectionChanged: true }, path: '/customerfrontend/check-details' };
-    checkChange('/customerfrontend')(request, genericResponse, nextFunction.next);
+    checkChange('/customerfrontend/')(request, genericResponse, nextFunction.next);
     assert.equal(genericResponse.address, '');
   });
 });
