703426b7934a0a118acbda5a7131be32209da738
praveen.kumar <praveen.kumar@engineering.digital.dwp.gov.uk>
2021-11-12 18:22:28 +0000

    chore(debug): Changes to enable remote debugging
    Signed-off-by: praveen.kumar <praveen.kumar@engineering.digital.dwp.gov.uk>

--- gulpfile.js/nodemon.js
+++ gulpfile.js/nodemon.js
@@ -4,6 +4,7 @@ function watchNodemon(cb) {
   let started = false;
   return nodemon({
     script: 'server.js',
+    nodeArgs: '--inspect=0.0.0.0:9229',
     ext: 'js, json, yaml, html',
     ignore: ['node_modules/', 'app/assets/', 'public/'],
   }).on('start', () => {
